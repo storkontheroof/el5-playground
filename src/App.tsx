c179f807513ca1c59b9da2b7ae64e3574eda781c
Richard van den Winkel <richard.van.den.winkel@e-wise.nl>
2024-07-25 04:45:46 +0200

    feat

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <div className="form-row">
+        <svg className="icon" fill="none" height="20" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><circle cx="11" cy="11" r="8"/><line x1="21" x2="16.65" y1="21" y2="16.65"/></svg>
+        <input className="text-input" type="text" placeholder="Username" />
+      </div>
+  )
+}
+
+export default App
